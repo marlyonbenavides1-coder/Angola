1aff22646e0b20bd3c38ee9994ac4c66140e6319
Marlyon808 <marlyonbenavides1@gmail.com>
2026-09-24 11:11:37 -0500

    Angola

---
+++ src/app/components/empleados/empleados.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-empleados',
+  styleUrl: './empleados.css',
+  templateUrl: './empleados.html',
+})
+export class Empleados {}
